3c2a57023637c376c461feb2af3c6901b2888da6
ndrtprv <ndrtprv@gmail.com>
2022-11-27 21:31:59 +0200

    Edited class Bank and classes Thread1(2) which extends class Thread which implements Runnable.

--- src/main/java/org/example/Thread2.java
+++ src/main/java/org/example/Thread2.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Thread2 extends Thread{
+public class Thread2 implements Runnable{
     private final Bank bank;
 
     public Thread2(Bank bank) {
@@ -9,6 +9,10 @@ public class Thread2 extends Thread{
 
     @Override
     public void run() {
-        bank.dec();
+        for (int i = 1; i <= 20000; i++) {
+            bank.setAccount(bank.getAccount() - 1);
+        }
+
+        System.out.println("Thread decrement: " + bank.getAccount());
     }
 }
--- src/main/java/org/example/ThreadsDemo.java
+++ src/main/java/org/example/ThreadsDemo.java
@@ -1,13 +1,17 @@
 package org.example;
 
 public class ThreadsDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final Bank bank = new Bank();
 
-        Thread1 thread1 = new Thread1(bank);
-        Thread2 thread2 = new Thread2(bank);
-
+        Thread thread1 = new Thread(new Thread1(bank));
         thread1.start();
+        Thread thread2 = new Thread(new Thread2(bank));
         thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(bank.getAccount());
     }
 }
